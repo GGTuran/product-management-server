42d71d8cc976ebb7ae6d9f1ce46bc58f864bd274
GGTuran <tayyabturan67@gmail.com>
2024-05-21 20:30:46 +0600

    managed to write the logic for inventory but still need to work on it

--- src/app/modules/orders/order.model.ts
+++ src/app/modules/orders/order.model.ts
@@ -1,27 +1,63 @@
 import { Schema, model } from "mongoose";
 import { TOrder } from "./order.interface";
+import { Product } from "../product/product.model";
 
-const  orderSchema = new Schema<TOrder>({
-    email:{
-        type:String,
-        required:[true, 'Email is required for ordering'],
+const orderSchema = new Schema<TOrder>({
+    email: {
+        type: String,
+        required: [true, 'Email is required for ordering'],
         // match:[/.+\@.+\..+/,'give a valid email'],
     },
-    productId:{
-        type:String,
-        ref:'Product',
-        required:[true, 'productId is required'],
+    productId: {
+        type: String,
+        ref: 'Product',
+        required: [true, 'productId is required'],
     },
-    price:{
-        type:Number,
-        required:[true, 'Price is required'],
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
         // min:[0,'Price has to be positive']
     },
-    quantity:{
-        type:Number,
-        required:[true, 'Quantity is required'],
+    quantity: {
+        type: Number,
+        required: [true, 'Quantity is required'],
         // min:[1,'At least one quantity should be brought'],
     },
 });
 
-export const Order = model<TOrder>('Order',orderSchema);
+
+
+
+orderSchema.pre('save', async function (next) {
+    const availability = await Product.findById(this.productId);
+    if (!availability) {
+        throw new Error("This product doesn't exists in database");
+    }
+    const { inventory:  quantity , }: any = await Product.findById(this.productId);
+    if (quantity < this.quantity) {
+        throw new Error("Insufficient number of products ordered");
+    };
+    //reducing the quantity
+    const updatedQuantity = await Product.findByIdAndUpdate(this.productId, {
+        $inc: {
+            'inventory.quantity':  - this.quantity,
+        },
+
+    },
+        { new: true },
+    );
+    //updating the stock if there is no quantity left
+    if(updatedQuantity?.inventory.quantity === 0){
+        await Product.findByIdAndUpdate(this.productId, {
+            $set:{
+                'inventory.inStock': false,
+            },
+        });
+    };
+    //calling the next function
+    next();
+});
+
+
+
+export const Order = model<TOrder>('Order', orderSchema);
